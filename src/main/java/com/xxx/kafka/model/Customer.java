196a395dcdd6ff41fb7fabd996a221b82330434a
alicedai <qingedaig@gmail.com>
2024-09-26 09:51:04 +1000

    Update gitignore and Customer.java

--- src/main/java/com/xxx/kafka/model/Customer.java
+++ src/main/java/com/xxx/kafka/model/Customer.java
@@ -2,10 +2,14 @@ package com.xxx.kafka.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Getter
+@Setter
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
